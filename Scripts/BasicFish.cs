a2c5164d9bc4b56fb08130c71e0eeb8d257bbc0c
StarkLyu <41030195+StarkLyu@users.noreply.github.com>
2019-12-08 19:49:12 +0800

    Current fish prefabs and fish swimming program

---
+++ Scripts/BasicFish.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicFish : MonoBehaviour
+{
+    public float life;
+    public float hunger;
+    public float speed;
+    public float turnSpeed;
+    public float floatSpeed;
+    public float verticalAngle;
+    public float verticalAdjustSpeed;
+    public Transform testTarget;
+    public bool testChase;
+    private Vector3 targetPos;
+    enum state
+    {
+        FREE,
+        ESCAPE,
+        FIND_FOOD,
+        CONTACT_PLAYER,
+        DEAD
+    }
+
+    enum MoveDir
+    {
+        FRONT, LEFT, RIGHT, UP, DOWN
+    }
+
+    private MoveDir direction;
+    private float dirTime;
+    private float lastTimestamp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = transform.position;
+        lastTimestamp = Time.time;
+        dirTime = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Swim();
+        if (testChase)
+        {
+            Chase(testTarget);
+        }
+        else
+        {
+            AdjustVerticleDir();
+            if (Time.time - lastTimestamp >= dirTime)
+            {
+                direction = (MoveDir)Random.Range(1, 5);
+                lastTimestamp = Time.time;
+                dirTime = Random.value * 3 + 3;
+                Debug.Log(direction);
+            }
+        }
+    }
+
+    private void AdjustVerticleDir()
+    {
+        Quaternion target = transform.rotation;
+        if (direction == MoveDir.UP)
+        {
+            target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y, verticalAngle);
+        }
+        else if (direction == MoveDir.DOWN)
+        {
+            target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y, -verticalAngle);
+        }
+        else
+        {
+            target = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y, 0);
+        }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target, verticalAdjustSpeed * Time.deltaTime);
+    }
+
+    private void Swim()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0, Space.Self);
+        float turnAngle = turnSpeed * Time.deltaTime;
+        switch (direction)
+        {
+            case MoveDir.FRONT:
+                break;
+            case MoveDir.LEFT:
+                transform.Rotate(0, -turnAngle, 0, Space.World);
+                break;
+            case MoveDir.RIGHT:
+                transform.Rotate(0, turnAngle, 0, Space.World);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    public void Chase(Transform target)
+    {
+        Vector3 targetDir = transform.InverseTransformPoint(target.position);
+        if (targetDir.z > 0)
+        {
+            direction = MoveDir.LEFT;
+        }
+        else if (targetDir.z < 0)
+        {
+            direction = MoveDir.RIGHT;
+        }
+        else if (targetDir.x < 0)
+        {
+            direction = Random.value < 0.5 ? MoveDir.LEFT : MoveDir.RIGHT;
+        }
+        else if (targetDir.x > 0)
+        {
+            direction = MoveDir.FRONT;
+        }
+        if (targetDir.y > 0)
+        {
+            transform.Rotate(0, 0, verticalAdjustSpeed * Time.deltaTime);
+        }
+        else if (targetDir.y < 0)
+        {
+            transform.Rotate(0, 0, -verticalAdjustSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Escape(Transform chaser)
+    {
+        Vector3 targetDir = transform.InverseTransformPoint(chaser.position);
+        if (targetDir.z < 0)
+        {
+            direction = MoveDir.LEFT;
+        }
+        else if (targetDir.z > 0)
+        {
+            direction = MoveDir.RIGHT;
+        }
+        else if (targetDir.x > 0)
+        {
+            direction = Random.value < 0.5 ? MoveDir.LEFT : MoveDir.RIGHT;
+        }
+        else if (targetDir.x < 0)
+        {
+            direction = MoveDir.FRONT;
+        }
+        if (targetDir.y < 0)
+        {
+            transform.Rotate(0, 0, verticalAdjustSpeed * Time.deltaTime);
+        }
+        else if (targetDir.y > 0)
+        {
+            transform.Rotate(0, 0, -verticalAdjustSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Scripts/FishGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishGenerator : MonoBehaviour
+{
+    public float generateRadius;
+    public int generateNum;
+    public List<string> fishes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < generateNum; i++)
+        {
+            GenerateOne(fishes[Random.Range(0, fishes.Count)]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateOne(string name)
+    {
+        string path = "FishPrefabs/" + name;
+        Instantiate((GameObject)Resources.Load(path), Random.insideUnitSphere * generateRadius, Quaternion.Euler(0, Random.value * 360, 0));
+    }
+}
